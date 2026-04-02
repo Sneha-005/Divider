0975df460d2fae31032d887b252b2ac24c728904
harsh singh <harsh2004416@gmail.com>
2026-04-02 22:08:54 +0530

    removed the example files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,23 @@
-import RootLayout from "@presentation/navigation/RootLayout";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
-export default RootLayout;
+export default function RootLayout() {
+  React.useEffect(() => {
+    console.log("[RootLayout] Layout mounted");
+  }, []);
+
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animationEnabled: true,
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
